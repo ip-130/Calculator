957c742a4914588acc2a115ebc62112d932dff47
Sergey <ip_130@mail.ru>
2021-06-20 11:03:49 +0300

    lifecycleactivity---> добавил в приложение функции обратного вызова и добавил лог в onStart

--- app/src/main/java/com/android/calculator/MainActivity.java
+++ app/src/main/java/com/android/calculator/MainActivity.java
@@ -1,14 +1,75 @@
 package com.android.calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LifeCycleActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find elements activity_main
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        makeToast("onStart");
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        makeToast("onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        makeToast("onPause");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState) {
+        super.onSaveInstanceState(saveInstanceState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        makeToast("onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        makeToast("onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        makeToast("onDestroy");
+    }
+
+    private void makeToast(String message){
+        Toast.makeText(getApplicationContext(), message,
+    Toast.LENGTH_SHORT).show();
+        Log.d(TAG, message);
     }
 }
