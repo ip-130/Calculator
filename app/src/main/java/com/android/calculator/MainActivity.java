84f52fb24badbbdbf89104d4bab58b48377a42ac
ip-130 <85837397+ip-130@users.noreply.github.com>
2021-06-22 17:44:18 +0300

    Change---> изменил название кнопок...

--- app/src/main/java/com/android/calculator/MainActivity.java
+++ app/src/main/java/com/android/calculator/MainActivity.java
@@ -1,35 +1,28 @@
 package com.android.calculator;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.os.PersistableBundle;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView calcDisplay;
-    private Button btnOne;
-    private Button btnTwo;
-    private Button btnThree;
-    private Button btnFour;
-    private Button btnFive;
-    private Button btnSix;
-    private Button btnSeven;
-    private Button btnEight;
-    private Button btnNine;
-    private Button btnNull;
+    private Button btn_0;
+    private Button btn_1;
+    private Button btn_2;
+    private Button btn_3;
+    private Button btn_4;
+    private Button btn_5;
+    private Button btn_6;
+    private Button btn_7;
+    private Button btn_8;
+    private Button btn_9;
     private Button btnPoint;
     private Button btnSum;
     private Button btnRazn;
@@ -41,12 +34,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button btnClearOneSymbol;
 
     private float first_value;
-    private float second_value;
     private char operation;
     private String str_numb = "";
-    private String str_numb_two = "";
 
-    private static final String Key = "KEY";
 
 
     @Override
@@ -58,16 +48,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ////обработка нажатий
         calcDisplay.setOnClickListener(this);
 
-        btnOne.setOnClickListener(this);
-        btnTwo.setOnClickListener(this);
-        btnThree.setOnClickListener(this);
-        btnFour.setOnClickListener(this);
-        btnFive.setOnClickListener(this);
-        btnSix.setOnClickListener(this);
-        btnSeven.setOnClickListener(this);
-        btnEight.setOnClickListener(this);
-        btnNine.setOnClickListener(this);
-        btnNull.setOnClickListener(this);
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
 
         btnPoint.setOnClickListener(this);
         btnSum.setOnClickListener(this);
@@ -82,27 +72,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void initialization() {
         //получение пользовательских элементов по идентификатору
-        calcDisplay = (TextView) findViewById(R.id.calcDisplay);
-        btnOne = (Button) findViewById(R.id.button_One);
-        btnTwo = (Button) findViewById(R.id.button_Two);
-        btnThree = (Button) findViewById(R.id.button_Three);
-        btnFour = (Button) findViewById(R.id.button_Four);
-        btnFive = (Button) findViewById(R.id.button_Five);
-        btnSix = (Button) findViewById(R.id.button_Six);
-        btnSeven = (Button) findViewById(R.id.button_Seven);
-        btnEight = (Button) findViewById(R.id.button_Eight);
-        btnNine = (Button) findViewById(R.id.button_Nine);
-        btnNull = (Button) findViewById(R.id.button_Null);
+        calcDisplay = (TextView) findViewById(R.id.calculatorDisplay);
 
-        btnPoint = (Button) findViewById(R.id.button_Point);
-        btnSum = (Button) findViewById(R.id.button_Sum);
-        btnRazn = (Button) findViewById(R.id.button_Razn);
-        btnDiv = (Button) findViewById(R.id.button_Div);
-        btnPow = (Button) findViewById(R.id.button_Pow);
-        btnEquals = (Button) findViewById(R.id.button_Equals);
+        btn_0 = (Button) findViewById(R.id.button_0);
+        btn_1 = (Button) findViewById(R.id.button_1);
+        btn_2 = (Button) findViewById(R.id.button_2);
+        btn_3 = (Button) findViewById(R.id.button_3);
+        btn_4 = (Button) findViewById(R.id.button_4);
+        btn_5 = (Button) findViewById(R.id.button_5);
+        btn_6 = (Button) findViewById(R.id.button_6);
+        btn_7 = (Button) findViewById(R.id.button_7);
+        btn_8 = (Button) findViewById(R.id.button_8);
+        btn_9 = (Button) findViewById(R.id.button_9);
+
+
+        btnPoint = (Button) findViewById(R.id.button_point);
+        btnSum = (Button) findViewById(R.id.button_add);
+        btnRazn = (Button) findViewById(R.id.button_sub);
+        btnDiv = (Button) findViewById(R.id.button_div);
+        btnPow = (Button) findViewById(R.id.button_mult);
+        btnEquals = (Button) findViewById(R.id.button_equals);
         btnPersent = (Button) findViewById(R.id.button_Persent);
-        btnClear = (Button) findViewById(R.id.button_Clear);
-        btnClearOneSymbol = (Button) findViewById(R.id.button_ClearOneSymbol);
+        btnClear = (Button) findViewById(R.id.button_clear);
+        btnClearOneSymbol = (Button) findViewById(R.id.button_clearOneSymbol);
     }
 
     @Override
@@ -110,13 +102,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onStart();
     }
 
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Bundle instanceState = null;
-        instanceState.putString(Key, calcDisplay.getText().toString());
-        instanceState.putParcelable(Key, (Parcelable) calcDisplay);
-    }
+//    @Override
+//    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        Bundle instanceState = null;
+//        instanceState.putString(Key, calcDisplay.getText().toString());
+//        instanceState.putParcelable(Key, (Parcelable) calcDisplay);
+//    }
 
     @Override
     protected void onResume() {
@@ -128,18 +120,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onPause();
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
-        super.onSaveInstanceState(saveInstanceState);
-        Bundle instanceState = null;
-        instanceState.putString(Key, calcDisplay.getText().toString());
-        instanceState.putParcelable(Key, (Parcelable) calcDisplay);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+//    @Override
+//    protected void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+//        super.onSaveInstanceState(saveInstanceState);
+//        Bundle instanceState = null;
+//        instanceState.putString(Key, calcDisplay.getText().toString());
+//        instanceState.putParcelable(Key, (Parcelable) calcDisplay);
+//    }
 
     @Override
     protected void onRestart() {
@@ -154,71 +141,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button_One:
-                first_value = 1;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Two:
-                first_value = 2;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Three:
-                first_value = 3;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Four:
-                first_value = 4;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Five:
-                first_value = 5;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Six:
-                first_value = 6;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Seven:
-                first_value = 7;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Eight:
-                first_value = 8;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Nine:
-                first_value = 9;
-                addNumber((int) first_value);
-                break;
-            case R.id.button_Null:
+            case R.id.button_0:
                 first_value = 0;
                 addNumber((int) first_value);
                 break;
-            case R.id.button_Point:
+            case R.id.button_1:
+                first_value = 1;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_2:
+                first_value = 2;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_3:
+                first_value = 3;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_4:
+                first_value = 4;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_5:
+                first_value = 5;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_6:
+                first_value = 6;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_7:
+                first_value = 7;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_8:
+                first_value = 8;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_9:
+                first_value = 9;
+                addNumber((int) first_value);
+                break;
+            case R.id.button_point:
                 point();
                 break;
-            case R.id.button_Sum:
+            case R.id.button_add:
                 mathOperation('+');
                 break;
-            case R.id.button_Razn:
+            case R.id.button_sub:
                 mathOperation('-');
                 break;
-            case R.id.button_Pow:
-                mathOperation('X');
+            case R.id.button_mult:
+                mathOperation('*');
                 break;
-            case R.id.button_Div:
+            case R.id.button_div:
                 mathOperation('/');
                 break;
-            case R.id.button_Clear:
-                clear();
+            case R.id.button_clear:
+                clearAll();
                 break;
             case R.id.button_Persent:
                 percent();
                 break;
-            case R.id.button_ClearOneSymbol:
+            case R.id.button_clearOneSymbol:
                 clearOneSymbol();
                 break;
-            case R.id.button_Equals:
+            case R.id.button_equals:
                 if (this.operation == '+' || this.operation == '-'
                         || this.operation == '/' || this.operation == 'X')
                     equalMethod();
@@ -238,23 +225,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         float res = 0;
         switch (this.operation) {
             case '+':
-                res = Float.parseFloat(this.str_numb_two) + Float.parseFloat(this.str_numb);
+                res = Float.parseFloat(this.str_numb) + Float.parseFloat(this.str_numb);
                 break;
             case '-':
-                res = Float.parseFloat(this.str_numb_two) - Float.parseFloat(this.str_numb);
+                res = Float.parseFloat(this.str_numb) - Float.parseFloat(this.str_numb);
                 break;
             case '/':
                 if (Float.parseFloat(this.str_numb) != 0)
-                    res = Float.parseFloat(this.str_numb_two) / Float.parseFloat(this.str_numb);
+                    res = Float.parseFloat(this.str_numb) / Float.parseFloat(this.str_numb);
                 else
                     Toast.makeText(this, "  null!!!", Toast.LENGTH_SHORT).show();
                 ;
                 break;
             case 'X':
-                res = Float.parseFloat(this.str_numb_two) * Float.parseFloat(this.str_numb);
+                res = Float.parseFloat(this.str_numb) * Float.parseFloat(this.str_numb);
                 break;
         }
-        clear();
+        clearAll();
         calcDisplay.setText(Float.toString(res));
     }
 
@@ -264,7 +251,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         this.first_value = 0;
     }
 
-    public void clear() {
+    public void clearAll() {
         calcDisplay.setText("0");
         this.str_numb = "";
         this.first_value = 0;
@@ -273,14 +260,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void addNumber(int number) {
         str_numb += Integer.toString(number);
-        /*if (first_value != 0) {
-            str_numb_two += Integer.toString(number);
-        }*/
-        calcDisplay.setText(str_numb_two);
+        calcDisplay.setText(str_numb);
     }
 
     public void mathOperation(char operation) {
-        str_numb_two = str_numb;
+//        str_numb_two = str_numb;
         this.first_value = Float.parseFloat(this.str_numb);
         calcDisplay.setText(String.valueOf(operation));
         this.str_numb = "";
